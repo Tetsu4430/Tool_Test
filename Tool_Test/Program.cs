dec0469943bbdd99d0d19099eb9d85ea7227b11f
yoshizawa-te <yoshizawa-te@YOSHIZAWA-TE-PC>
2024-05-28 10:36:47 +0900

    テスト

--- Tool_Test/Program.cs
+++ Tool_Test/Program.cs
@@ -17,6 +17,7 @@ namespace Tool_Test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //テスト
         }
     }
 }
